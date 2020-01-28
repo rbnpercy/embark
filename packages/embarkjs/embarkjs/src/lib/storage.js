3c8a51d3ad600aedb60f8c0aeddca32070135382
emizzle <ericmastro@status.im>
2020-01-28 13:52:15 +1100

    Refactor Storage to ES6 class

--- packages/embarkjs/embarkjs/src/lib/storage.js
+++ packages/embarkjs/embarkjs/src/lib/storage.js
@@ -1,106 +1,104 @@
 import {detectSeries} from './async';
 require('colors');
 
-const Storage = {};
+export default class Storage {
+  static Providers = {};
+  static noProviderError = 'Storage provider not set; e.g EmbarkJS.Storage.setProvider("ipfs")';
 
-Storage.Providers = {};
-Storage.noProviderError = 'Storage provider not set; e.g EmbarkJS.Storage.setProvider("ipfs")';
-
-Storage.saveText = function (text) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.saveText(text);
-};
-
-Storage.get = function (hash) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.get(hash);
-};
-
-Storage.uploadFile = function (inputSelector) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.uploadFile(inputSelector);
-};
-
-Storage.getUrl = function (hash) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.getUrl(hash);
-};
-
-Storage.resolve = function (name, callback) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.resolve(name, callback);
-};
-
-Storage.register = function (addr, callback) {
-  if (!this.currentStorage) {
-    throw new Error(this.noProviderError);
-  }
-  return this.currentStorage.register(addr, callback);
-};
-
-Storage.registerProvider = function (providerName, obj) {
-  this.Providers[providerName] = obj;
-};
-
-Storage.setProvider = function (providerName, options) {
-  let provider = this.Providers[providerName];
-
-  if (!provider) {
-    if (providerName === 'ipfs') {
-      console.log("the embarkjs-ipfs package might be missing from your project dependencies");
+  static saveText (text) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
     }
-    if (providerName === 'swarm') {
-      console.log("the embarkjs-swarm package might be missing from your project dependencies");
+    return this.currentStorage.saveText(text);
+  }
+
+  static get (hash) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
     }
-    throw new Error('Unknown storage provider: ' + providerName);
+    return this.currentStorage.get(hash);
   }
 
-  this.currentProviderName = providerName;
-  this.currentStorage = provider;
-
-  return provider.setProvider(options);
-};
-
-Storage.isAvailable = function () {
-  if (!this.currentStorage) {
-    return Promise.resolve(false);
+  static uploadFile (inputSelector) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
+    }
+    return this.currentStorage.uploadFile(inputSelector);
   }
-  return this.currentStorage.isAvailable();
-};
 
-// TODO: most of this logic should move to the provider implementations themselves
-Storage.setProviders = function (dappConnOptions, addlOpts) {
-  const self = this;
-  detectSeries(dappConnOptions, (dappConn, callback) => {
-    let options = dappConn;
-    if (dappConn === '$BZZ') options = {"useOnlyGivenProvider": true};
-    options = {...options, ...addlOpts, ...dappConn};
-    try {
-      self.setProvider(dappConn === '$BZZ' ? dappConn : dappConn.provider, options).then(() => {
-        self.isAvailable().then((isAvailable) => {
-          callback(null, isAvailable);
+  static getUrl (hash) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
+    }
+    return this.currentStorage.getUrl(hash);
+  }
+
+  static resolve (name, callback) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
+    }
+    return this.currentStorage.resolve(name, callback);
+  }
+
+  static register (addr, callback) {
+    if (!this.currentStorage) {
+      throw new Error(this.noProviderError);
+    }
+    return this.currentStorage.register(addr, callback);
+  }
+
+  static registerProvider (providerName, obj) {
+    this.Providers[providerName] = obj;
+  }
+
+  static setProvider (providerName, options) {
+    let provider = this.Providers[providerName];
+
+    if (!provider) {
+      if (providerName === 'ipfs') {
+        console.log("the embarkjs-ipfs package might be missing from your project dependencies");
+      }
+      if (providerName === 'swarm') {
+        console.log("the embarkjs-swarm package might be missing from your project dependencies");
+      }
+      throw new Error('Unknown storage provider: ' + providerName);
+    }
+
+    this.currentProviderName = providerName;
+    this.currentStorage = provider;
+
+    return provider.setProvider(options);
+  }
+
+  static isAvailable () {
+    if (!this.currentStorage) {
+      return Promise.resolve(false);
+    }
+    return this.currentStorage.isAvailable();
+  }
+
+  // TODO: most of this logic should move to the provider implementations themselves
+  static setProviders (dappConnOptions, addlOpts) {
+    const self = this;
+    detectSeries(dappConnOptions, (dappConn, callback) => {
+      let options = dappConn;
+      if (dappConn === '$BZZ') options = { "useOnlyGivenProvider": true };
+      options = { ...options, ...addlOpts, ...dappConn };
+      try {
+        self.setProvider(dappConn === '$BZZ' ? dappConn : dappConn.provider, options).then(() => {
+          self.isAvailable().then((isAvailable) => {
+            callback(null, isAvailable);
+          }).catch(() => {
+            callback(null, false);
+          });
         }).catch(() => {
-          callback(null, false);
+          callback(null, false); // catch errors for when bzz object not initialised but config has requested it to be used
         });
-      }).catch(() => {
-        callback(null, false); // catch errors for when bzz object not initialised but config has requested it to be used
-      });
-    } catch (err) {
-      callback(null, false);
-    }
-  }, function (err, result) {
-    if (!result) console.log('Could not connect to a storage provider using any of the dappConnections in the storage config'.red);
-  });
-};
-
-export default Storage;
+      } catch (err) {
+        callback(null, false);
+      }
+    }, function (err, result) {
+      if (!result) console.log('Could not connect to a storage provider using any of the dappConnections in the storage config'.red);
+    });
+  }
+}
